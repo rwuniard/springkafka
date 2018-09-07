58419af3d2861b853d4e325da9f9d272a18057ff
Ronson Wuniardy <rwuniard@hotmail.com>
2018-09-07 14:07:20 -0400

    Adding multiple kafkaTemplate to support String, String and String, People.

--- src/main/java/com/example/config/MessageKafkaConfig.java
+++ src/main/java/com/example/config/MessageKafkaConfig.java
@@ -1,6 +1,5 @@
 package com.example.config;
 
-import com.example.model.People;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -8,26 +7,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class KafkaConfig {
+public class MessageKafkaConfig {
+
     @Bean
-    public ProducerFactory<String, People> producerFactory() {
+    public ProducerFactory<String, String> producerFactory() {
 
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-        return new DefaultKafkaProducerFactory(config);
+        return new DefaultKafkaProducerFactory<>(config);
     }
 
     @Bean
-    public KafkaTemplate<String, People> kafkaTemplate() {
+    public KafkaTemplate<String, String> messageKafkaTemplate() {
 
         return new KafkaTemplate<>(producerFactory());
     }
--- src/main/java/com/example/controller/PublishMessageController.java
+++ src/main/java/com/example/controller/PublishMessageController.java
@@ -14,27 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class PublishMessageController {
 
 
-    //@Autowired
-    //private KafkaTemplate<String, String> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
-    private KafkaTemplate<String, People> kafkaTemplate;
+    private KafkaTemplate<String, People> peopleKafkaTemplate;
 
 
     @RequestMapping(value="publish/{message}", method= RequestMethod.GET)
     public String post(@PathVariable String message) {
-        kafkaTemplate.send("test", "key", new People(message, message));
+        kafkaTemplate.send("test", "key", message);
 
         return "Published successfully";
     }
 
-    /*
-    @RequestMapping(value="publish/{name}", method=RequestMethod.GET)
+
+    @RequestMapping(value="publish/name/{name}", method=RequestMethod.GET)
     public String postUser(@PathVariable String name) {
-        kafkaTemplate.send("test", "key", new People(name, name));
+        peopleKafkaTemplate.send("test", "key", new People(name, name));
         return "Published People successfully";
     }
-*/
+
     @RequestMapping(value="hello", method=RequestMethod.GET)
     public String hello() {
         return "hello";
